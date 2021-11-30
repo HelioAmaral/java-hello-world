efe9a62bb6f093ff9b2eccc6a11516b8849502bc
Helio Amaral <helioamaral@Helios-MacBook-Air.local>
2021-11-30 16:13:32 +0100

    Initial commit

---
+++ src/com/javahelloworld/Main.java
@@ -0,0 +1,10 @@
+package com.javahelloworld;
+
+public class Main {
+
+    public static void main(String[] args) {
+	String name = "Hello";
+    String name1 = "World!";
+    System.out.println(name + " " + name1);
+    }
+}
